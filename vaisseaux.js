b3c65dad45dd54c06beb664778421d657175846e
argonaultes <gael@argonaultes.fr>
2024-11-05 15:09:21 +0100

    List usage

--- vaisseaux.js
+++ vaisseaux.js
@@ -5,6 +5,10 @@ class Vaisseau {
         this.couleur = couleur;
         this.latence = latence;
     }
+
+    compareVaisseau(other) {
+        return this.nom == other.nom;
+    }
 }
 
 
@@ -14,17 +18,30 @@ class Circuit {
         this.distance_tour = distance_tour;
         this.vaisseaux = new Set();
         this.vaisseauxMap = new Map();
+        this.vaisseauxList = [];
+    }
+
+    subscribe(vaisseau) {
+        this.subscribeList(vaisseau);
     }
 
     //Set usage
-    subscribe(vaisseau) {
+    subscribeSet(vaisseau) {
         this.vaisseaux.add(JSON.stringify(vaisseau));
-        // pour repasser dans l'autre sens JSON.parse
     }
 
     //Map usage
     subscribeMap(vaisseau) {
         this.vaisseauxMap.set(vaisseau.nom, vaisseau);
+    }
+
+    subscribeList(vaisseau) {
+        //tester si le vaisseau est dans la liste
+        if (!this.vaisseauxList.find((element) => vaisseau.compareVaisseau(element))) {
+            this.vaisseauxList.push(vaisseau);
+        }
+        //si oui, ne rien faire
+        //sinon, ajouter le vaisseau à la liste
     }
 
 }
@@ -35,10 +52,10 @@ const vs = [
 ];
 
 const melbourne = new Circuit(5, 1300);
-melbourne.subscribeMap(vs[0]);
-melbourne.subscribeMap(vs[0]);
-melbourne.subscribeMap(vs[1]);
-melbourne.subscribeMap(new Vaisseau(vs[0].nom, vs[0].couleur, vs[0].vitesse, vs[0].latence));
+melbourne.subscribe(vs[0]);
+melbourne.subscribe(vs[0]);
+melbourne.subscribe(vs[1]);
+melbourne.subscribe(new Vaisseau(vs[0].nom, vs[0].couleur, vs[0].vitesse, vs[0].latence));
 
 console.log(melbourne);
 
